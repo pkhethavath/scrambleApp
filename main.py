7139ab206b721d2f35d79c9947aeb620fe58b9b0
doyel pal <doyel pal>
2016-06-10 12:28:34 -0700

    working code

--- main.py
+++ main.py
@@ -19,42 +19,17 @@ import os
 import jinja2
 import random
 import array
-def doSomething(old):
-    new = old.upper()
-    return(new)
-
-
-def pig_latinize_word(word):
-  vowels = ["a", "e", "i", "o", "u"]
-  if word[0].lower() in vowels:
-    pig_latin_word = word + "ay"
-  else:
-    if word[1].lower() not in vowels:
-      first_letters = word[:2]
-      rest_of_word = word[2:]
-      pig_latin_word = rest_of_word + first_letters + "ay"
-    else:
-      first_letter = word[0]
-      rest_of_word = word[1:]
-      pig_latin_word = rest_of_word + first_letter + "ay"
-  return pig_latin_word
-
-
-def pig_latinize_phrase(phrase):
-  words = phrase.split(" ")
-  pl_words = []
-  for word in words:
-    pl_words.append(pig_latinize_word(word))
-  final_string = " ".join(pl_words)
-  return final_string
-
 current_directory=jinja2.Environment(
-  loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 def selectWord():
     library = ["HOUSE",
              "MOUNTAIN",
              "JOUST",
+             "CATTLE",
+             "YOUNG",
+             "ROUTE",
+             "COMPUTER",
              "ACTION",
              "CARD"]
     n=len(library)
@@ -74,34 +49,24 @@ def permute(s1):
     print len(t)
     return t
 
-class MainHandler(webapp2.RequestHandler):
 
+class MainHandler(webapp2.RequestHandler):
+    orig_word=""
     def get(self):
-        hello_template = current_directory.get_template("templates/index.html")
+        prog_template = current_directory.get_template("templates/index.html")
         orig_word = selectWord()
         new_word = permute(orig_word)
-        self.response.write(hello_template.render({"new_word":new_word}))
+        self.response.write(prog_template.render({"new_word":new_word,"orig_word":orig_word}))
     def post(self):
-        results_template=current_directory.get_template("templates/results.html")
-        the_name = self.request.get('first_name')
-        the_city = self.request.get('user_city')
-        new_name=doSomething(the_name)
-        new_city=doSomething(the_city)
-        my_dict={'loud_name':new_name, 'loud_city':new_city}
-        self.response.write(results_template.render(my_dict))
-
-class PigHandler(webapp2.RequestHandler):
-    def get(self):
-        pig_template=current_directory.get_template("templates/pig.html")
-        self.response.write(pig_template.render())
-    def post(self):
-        pig_template=current_directory.get_template("templates/pig.html")
-        new_word=pig_latinize_phrase(self.request.get("input_word"))
-        my_vars={'final_word':new_word}
-        self.response.write(pig_template.render(my_vars))
-
+        prog_template=current_directory.get_template("templates/index.html")
+        guess_word = self.request.get("answer")
+        orig_word=self.request.get("clue")
+        self.response.write(prog_template.render({"new_word":guess_word}))
+        if guess_word == orig_word:
+            self.response.write("<br><br><h2>You Got it!</h2>")
+        else:
+            self.response.write("<br><br><h2>SORRY!</h2>")
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ('/piglatin',PigHandler)
-], debug=True)
+    ('/', MainHandler)
+    ], debug=True)
